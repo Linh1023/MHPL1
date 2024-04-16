96022f5e00e45b8e05ff6ab1bc452e3a7ebca820
trancuong19022 <113131294+trancuong19022@users.noreply.github.com>
2024-04-16 16:30:02 +0700

    Update BLL_ThongTinSD.java

--- src/main/java/com/mycompany/qlthanhvien/BLL/BLL_ThongTinSD.java
+++ src/main/java/com/mycompany/qlthanhvien/BLL/BLL_ThongTinSD.java
@@ -2,142 +2,87 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.qlthanhvien.DAL;
+package com.mycompany.qlthanhvien.BLL;
 
-import com.mycompany.qlthanhvien.BLL.ThongTinSD;
+import com.mycompany.qlthanhvien.DAL.DAL_ThongTinSD;
 import java.util.List;
-import org.hibernate.Session;
-import com.mycompany.qlthanhvien.BLL.ThanhVien;
-import java.util.Date;
-import java.util.List;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Join;
-import javax.persistence.criteria.JoinType;
-import javax.persistence.criteria.Root;
-import org.hibernate.Session;
-public class DAL_ThongTinSD {
-    private Session session;
+
+/**
+ *
+ * @author PC
+ */
+public class BLL_ThongTinSD {
     
-    public DAL_ThongTinSD (){
-         session = HibernateUtils.getSessionFactory().openSession();
+    DAL_ThongTinSD dAL_ThongTinSD;
+    
+    public BLL_ThongTinSD () {
+        dAL_ThongTinSD= new DAL_ThongTinSD();
     }
-   
-     public List<ThongTinSD> getThongTinSD() {
-         
-         List<ThongTinSD> thongTinSDs;
-         session.beginTransaction();
-        thongTinSDs = this.session.createQuery("FROM ThongTinSD WHERE  MaTB is not NULL").list();
-        session.getTransaction().commit();
+    
+    public List<ThongTinSD> getThongTinSD () {
+        List<ThongTinSD> thongTinSDs = dAL_ThongTinSD.getThongTinSD();
         return thongTinSDs;
     }
-     
+    public List<ThongTinSD> getListThongTinSD () {
+        List<ThongTinSD> thongTinSDs = dAL_ThongTinSD.getListThongTinSD();
+        return thongTinSDs;
+    }
+    public List<ThongTinSD> searchThongTinSD_MaTV_MaTB (String col, String value) {
+        List<ThongTinSD> thongTinSDs = dAL_ThongTinSD.searchThongTinSD_MaTV_MaTB(col, value);
+        return thongTinSDs;
+    }
+    
+    public Object[][] convertListThongTinSD (List<ThongTinSD> thongTinSDs) {
+        int rows = thongTinSDs.size();
+        int cols = 8;
+         Object[][] objectses = new Object[rows][cols];
+         for (int i = 0; i<rows; i++) {
+             objectses[i][0] = thongTinSDs.get(i).getMaTT();
+             objectses[i][1] = thongTinSDs.get(i).getMaTV();
+             objectses[i][2] = thongTinSDs.get(i).getThanhVien().getHoten();
+             objectses[i][3] = thongTinSDs.get(i).getMaTB();
+             objectses[i][4] = thongTinSDs.get(i).getThietBi().getTenTB();
+             objectses[i][5] = thongTinSDs.get(i).getTGMuon();
+             objectses[i][6] = thongTinSDs.get(i).getTGTra();
+             if (objectses[i][6] == null) {
+                 objectses[i][7] = "Đang mượn";
+             } else {
+                  objectses[i][7] = "Đã trả";
+             }
+         }
+         return objectses;
+    }
+    
     public void addThongTinSD(ThongTinSD thongTinSD)
     {
-        session.beginTransaction();
-        session.save(thongTinSD);   
-        session.getTransaction().commit();
+       
+        dAL_ThongTinSD.addThongTinSD(thongTinSD);   
+        
         
     }
     public void updateThongTinSD(ThongTinSD thongTinSD)
     {
-       session.beginTransaction();
-       session.merge(thongTinSD);
-       session.getTransaction().commit();
+       
+       dAL_ThongTinSD.updateThongTinSD(thongTinSD);
+       
     }
     public void deleteThongTinSD(ThongTinSD thongTinSD)
     {
-        session.beginTransaction();
-        session.delete(thongTinSD);
-        session.getTransaction().commit();
+       
+        dAL_ThongTinSD.deleteThongTinSD(thongTinSD);
+   
     }
     
-    public List<ThongTinSD> searchThongTinSD_MaTV_MaTB(String col, String value) {
-         
-         List<ThongTinSD> thongTinSDs;
-         session.beginTransaction();
-        thongTinSDs = this.session.createQuery("FROM ThongTinSD WHERE  MaTB is not NULL AND " + col + "=" + value  ).list();
-        session.getTransaction().commit();
-        return thongTinSDs;
+    public List getThanhVienTheoTG(){
+        return dAL_ThongTinSD.getThanhVienTheoTG();
     }
-     public List getThanhVienTheoTG() {
-        CriteriaBuilder builder = session.getCriteriaBuilder();
-        CriteriaQuery<Object[]> criteriaQuery = builder.createQuery(Object[].class);
-        Root<ThongTinSD> root = criteriaQuery.from(ThongTinSD.class);
-
-        criteriaQuery.multiselect(builder.countDistinct(root.get("maTV")), builder.function("date", Date.class, root.get("tGVao")));
-        criteriaQuery.where(builder.isNotNull(root.get("tGVao")));
-        criteriaQuery.groupBy(builder.function("date", Date.class, root.get("tGVao")));
-
-        List<Object[]> results = session.createQuery(criteriaQuery).getResultList();
-        for (Object[] result : results) {
-            Long soluong = (Long) result[0];
-            Date date = (Date) result[1];
-            System.out.println("So luong: " + soluong + ", date: " + date.toString());
-        }
-        return results;
-    }
-
+    
     public List getThanhVienTheoKhoa() {
-        CriteriaBuilder builder = session.getCriteriaBuilder();
-        CriteriaQuery<Object[]> criteriaQuery = builder.createQuery(Object[].class);
-        Root<ThongTinSD> root = criteriaQuery.from(ThongTinSD.class);
-        Join<ThongTinSD, ThanhVien> join = root.join("thanhVien", JoinType.INNER);
-        
-        criteriaQuery.multiselect(builder.countDistinct(join.get("maTV")),join.get("khoa"));
-        criteriaQuery.where(builder.isNotNull(root.get("tGVao")));
-        criteriaQuery.groupBy(join.get("khoa"));
-        
-        List<Object[]> results = session.createQuery(criteriaQuery).getResultList();
-        for (Object[] result : results) {
-            Long soluong = (Long) result[0];
-            String khoa = (String) result[1];
-            System.out.println("So luong: " + soluong + ", date: " + khoa);
-        }
-        return results;
+        return dAL_ThongTinSD.getThanhVienTheoKhoa();
     }
     
     public List getThanhVienTheoNganh() {
-        CriteriaBuilder builder = session.getCriteriaBuilder();
-        CriteriaQuery<Object[]> criteriaQuery = builder.createQuery(Object[].class);
-        Root<ThongTinSD> root = criteriaQuery.from(ThongTinSD.class);
-        Join<ThongTinSD, ThanhVien> join = root.join("thanhVien", JoinType.INNER);
-        
-        criteriaQuery.multiselect(builder.countDistinct(join.get("maTV")),join.get("nganh"));
-        criteriaQuery.where(builder.isNotNull(root.get("tGVao")));
-        criteriaQuery.groupBy(join.get("nganh"));
-        
-        List<Object[]> results = session.createQuery(criteriaQuery).getResultList();
-        for (Object[] result : results) {
-            Long soluong = (Long) result[0];
-            String nganh = (String) result[1];
-            System.out.println("So luong: " + soluong + ", nganh: " + nganh);
-        }
-        return results;
+        return dAL_ThongTinSD.getThanhVienTheoNganh();
     }
     
-     public List<ThongTinSD> getListThongTinSD() {
-         
-         List<ThongTinSD> thongTinSDs;
-         session.beginTransaction();
-        thongTinSDs = this.session.createQuery("FROM ThongTinSD WHERE  TGVao is not NULL").list();
-        session.getTransaction().commit();
-        return thongTinSDs;
-    }
-     
-     
-     public static void main(String[] args) {
-        DAL_ThongTinSD dAL_ThongTinSD = new DAL_ThongTinSD();
-//        ThongTinSD thongTinSD = new ThongTinSD();
-//        
-//        thongTinSD.setMaTV(1123330257);
-//        
-//        
-//        dAL_ThongTinSD.addThongTinSD(thongTinSD);
-            
-        List<ThongTinSD> thongTinSDs = dAL_ThongTinSD.getListThongTinSD();
-        for (int i =  0; i<thongTinSDs.size(); i++) {
-            System.out.println(thongTinSDs.get(i).getThanhVien().getHoten());
-        }
-    }
 }
