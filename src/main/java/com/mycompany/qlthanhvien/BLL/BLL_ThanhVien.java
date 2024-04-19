2937cec2ab82cfac6eb6f4d312ede390afa289ae
Lochoang21 <115520127+Lochoang21@users.noreply.github.com>
2024-04-19 21:23:53 +0700

    Update BLL_ThanhVien.java

--- src/main/java/com/mycompany/qlthanhvien/BLL/BLL_ThanhVien.java
+++ src/main/java/com/mycompany/qlthanhvien/BLL/BLL_ThanhVien.java
@@ -19,58 +19,62 @@ import org.hibernate.cfg.Configuration;
  */
 public class BLL_ThanhVien {
     private DAL_ThanhVien thanhVienDAL;
-//    private static SessionFactory factory;
+    // private static SessionFactory factory;
 
-    public BLL_ThanhVien () {
-        thanhVienDAL= new DAL_ThanhVien();
+    public BLL_ThanhVien() {
+        thanhVienDAL = new DAL_ThanhVien();
     }
-    
-    public List<ThanhVien> getThanhVien () {
+
+    public List<ThanhVien> getThanhVien() {
         List<ThanhVien> thongTinSDs = thanhVienDAL.getThanhvien();
         return thongTinSDs;
     }
-    public List<ThanhVien> SearchThanhVien (String column, String value) {
+
+    public List<ThanhVien> SearchThanhVien(String column, String value) {
         List<ThanhVien> thongTinSDs = thanhVienDAL.SearchThanhvien(column, value);
         return thongTinSDs;
     }
-public void addThanhVien(ThanhVien thongTinSD)
-    {
-       
-        thanhVienDAL.addThanhVien(thongTinSD);   
-        
-        
+
+    public void addThanhVien(ThanhVien thongTinSD) {
+
+        thanhVienDAL.addThanhVien(thongTinSD);
+
     }
-    public void updateThanhVien(ThanhVien thongTinSD)
-    {
-       
-       thanhVienDAL.updateThanhVien(thongTinSD);
-       
+
+    public void updateThanhVien(ThanhVien thongTinSD) {
+
+        thanhVienDAL.updateThanhVien(thongTinSD);
+
     }
-    public void deleteThanhVien(ThanhVien thongTinSD)
-    {
-       
+
+    public void deleteThanhVien(ThanhVien thongTinSD) throws Exception {
+
         thanhVienDAL.deleteThanhVien(thongTinSD);
-   
+
     }
+
     public boolean importExcel(ArrayList<ThanhVien> danhsachThanhvien) {
-        //System.out.println("tới đây rồi");
+        // System.out.println("tới đây rồi");
         int a = 0;
         for (ThanhVien thanhVien : danhsachThanhvien) {
             thanhVienDAL.mergeThanhVien(thanhVien);
-        //    System.out.println(a);
+            // System.out.println(a);
             a++;
         }
         return true;
     }
-     // Gọi hàm từ DAL để lấy danh sách các năm nhập học
+
+    // Gọi hàm từ DAL để lấy danh sách các năm nhập học
     public List<Integer> getDistinctYears() {
         return thanhVienDAL.getDistinctYears();
     }
+
     public List<ThanhVien> getThanhVienByYear(int year) {
         return thanhVienDAL.getThanhVienByYear(year);
     }
-    public void deleteThanhVienByYear(int year) {
-    thanhVienDAL.deleteThanhVienByYear(year);
-}
 
-  }
+    public boolean deleteThanhVienByYear(int year) {
+        return thanhVienDAL.deleteThanhVienByYear(year);
+    }
+
+}
